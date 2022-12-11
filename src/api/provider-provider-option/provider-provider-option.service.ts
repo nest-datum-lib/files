ff38fb0e3da15fed24aba78ad2b632a539e507c0
ihor <ihor.bielchenko@gmail.com>
2022-12-11 04:41:31 +0100

    test

--- src/api/provider-provider-option/provider-provider-option.service.ts
+++ src/api/provider-provider-option/provider-provider-option.service.ts
@@ -87,6 +87,7 @@ export class ProviderProviderOptionService extends SqlService {
 		try {
 			this.cacheService.clear([ 'provider', 'option', 'relation', 'many' ]);
 			this.cacheService.clear([ 'provider', 'option', 'relation', 'one', id ]);
+			this.cacheService.clear([ 'provider', 'one' ]);
 
 			await this.providerProviderOptionRepository.delete({ id });
 
