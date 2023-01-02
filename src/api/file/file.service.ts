178bfba83dafbaa31fdff4dfaad5046d0ccabe4a
ihor <ihor.bielchenko@gmail.com>
2023-01-03 00:08:21 +0100

    test

--- src/api/file/file.service.ts
+++ src/api/file/file.service.ts
@@ -193,6 +193,8 @@ export class FileService extends SqlService {
 			this.cacheService.clear([ 'file', 'many' ]);
 			this.cacheService.clear([ 'file', 'one' ]);
 
+			console.log('?????????')
+
 			if (!payload['path']) {
 				const systemOptionContent = await this.systemSystemSystemOptionRepository.findOne({
 					select: {
