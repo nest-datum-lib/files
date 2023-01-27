10350a8e21d33e53bb8a563e625cc213379924e8
ihor <ihor.bielchenko@gmail.com>
2023-01-27 21:06:58 +0100

    test

--- queue/download.processor.ts
+++ queue/download.processor.ts
@@ -33,6 +33,8 @@ export class DownloadProcessor extends QueueService {
 		private readonly cacheService: CacheService,
 	) {
 		super(queueRepository);
+
+		console.log('fileType.fileTypeFromFile', fileType.fileTypeFromFile);
 	}
 
 	async callback(payload: object, currentTime): Promise<any> {
