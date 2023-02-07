1e8398be62b02a5f779078c7e4a10b4a18a5d1e4
ihor <ihor.bielchenko@gmail.com>
2023-02-07 06:42:35 +0100

    Test logic.

--- src/main.ts
+++ src/main.ts
@@ -19,24 +19,32 @@ process.on('warning', onWarning);
 process.on('uncaughtException', onUncaughtException);
 
 async function bootstrap() {
-	const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+	const appTcp = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
 		strategy: new CustomServerTCP({
 			host: process.env.APP_HOST,
 			port: Number(process.env.APP_PORT),
 		}),
 	});
+	const appHttp = await NestFactory.create(AppModule);
 	const transport = await NestFactory.create(TransportModule);
 	const transportService = transport.get(TransportService);
 
 	try {
-		await transportService.create();
-		await app.listen()
-		await transport.close();
+		appHttp.enableCors();
 
-		console.log(`Successfuly started on "${process.env.APP_HOST}:${process.env.APP_PORT}".`);
+		await transportService.create();
+		await appHttp.listen(Number(process.env.APP_HTTP_PORT), async () => {
+			console.log('HTTP service listening on port:', process.env.APP_HTTP_PORT);
+			console.log('Replica listening on port:', process.env.APP_PORT);
+
+			await appTcp.listen();
+			await transport.close();
+
+			console.log(`Successfuly started on "${process.env.APP_HOST}:${process.env.APP_PORT}".`);
+		});
 	}
 	catch (err) {
-		await app.close();
+		await appTcp.close();
 		
 		console.error(err.message);
 	}
